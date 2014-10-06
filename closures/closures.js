324b8d83ed6a7c72c21535d11473cc90e873753b
Sam Weber <sweber@esi-estech.com>
2014-10-06 11:37:23 -0500

    example added

--- closures/closures.js
+++ closures/closures.js
@@ -37,6 +37,39 @@ while (data.length) {
 }
 
 
+
+console.log('***********************************');
+
+var blarg;
+var fcn;
+
+(function() {
+  var baz = 42;
+
+  blarg = function() {
+    var value = 27;
+
+    fcn = function() {
+      console.log(value);
+      console.log(baz);
+    };
+  };
+})();
+
+if (typeof value === 'undefined') {
+  console.log('value is undefined ... as expected');
+}
+else {
+  console.log(typeof value);
+}
+
+blarg();
+fcn();
+
+console.log('***********************************');
+
+
+
 // if we don't care about the return value of the IIFE, we
 // can use a variety of symbols in front of the IIFE to
 // make it a little prettier but still be immediately
@@ -60,21 +93,23 @@ while (data.length) {
 
 // strict mode doesn't allow 'with'
 // This code, however, works properly
-//for (i = 0; i < 5; i++) {
-//  with ({num: i}) {
-//    data[i] = function foo() {
-//      console.log('data[%s] = %s', num, num);
-//    };
-//  }
-//}
-//data[0](); data[1](); data[2](); data[3](); data[4]();
+/*
+  for (i = 0; i < 5; i++) {
+    with ({num: i}) {
+      data[i] = function foo() {
+        console.log('data[%s] = %s', num, num);
+      };
+    }
+  }
+  data[0](); data[1](); data[2](); data[3](); data[4]();
+*/
 
 
 // invocation: node -harmony app.js
-for (i = 0; i < 5; i++) {
-   let num = i;
-   data[i] = function foo() {
-     console.log('data[%s] = %s', num, num);
-   };
-}
-data[0](); data[1](); data[2](); data[3](); data[4]();
+//for (i = 0; i < 5; i++) {
+//   let num = i;
+//   data[i] = function foo() {
+//     console.log('data[%s] = %s', num, num);
+//   };
+//}
+//data[0](); data[1](); data[2](); data[3](); data[4]();
