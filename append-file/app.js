a9471139c6c45ba7b17ca3eddf3d55d253b2cbd8
Sam Weber <sweber@esi-estech.com>
2014-10-13 16:26:10 -0500

    clarification

--- append-file/app.js
+++ append-file/app.js
@@ -5,7 +5,15 @@
 // below, however, the console and stuff.txt have the lines in the
 // expected order.
 // Also interesting is that this out-of-order behavior persists
-// across Node.js versions on the Mac but does not occur on Linux.
+// across Node.js versions on the Mac.  This behavior seems rarer
+// on Linux but does occur.
+//
+// This issue was reported on Github (#8544) on 10/13/2014.  The concensus
+// there is that this behavior is "works as designed."
+// According to Christopher Watford: "this is due to the asynchronous 
+// behavior of appendFile. While each of the calls to logIt() will 
+// occur in-order, the actual appending is not guaranteed to occur 
+// in-order when called asynchronously."
 
 'use strict';
 
