e77aa5cfdc671f42239f8d81097dda0af174d900
Sam Weber <sweber@esi-estech.com>
2014-10-13 08:12:14 -0500

    text fix up

--- append-file/app.js
+++ append-file/app.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-// Surprisingly, the lines output below do not appear in order
-// in the stuff.txt file.  If you uncomment the else clause below,
-// however, the console and stuff.txt have the lines the the
+// Surprisingly, on my Mac, the lines output below do not appear 
+// in order in the stuff.txt file.  If you uncomment the else clause
+// below, however, the console and stuff.txt have the lines in the
 // expected order.
 // Also interesting is that this out-of-order behavior persists
 // across Node.js versions on the Mac but does not occur on Linux.
